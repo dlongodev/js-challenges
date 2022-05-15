d671c40856c2aabd9d3ee3880309b42b499fe53a
dlongo <dlongodesign@gmail.com>
2022-05-15 19:46:59 -0400

    arrays similar

--- day_02.js
+++ day_02.js
@@ -1,5 +1,28 @@
 // PREP => Parameter, Return, Example, Pseudo
+//------------------------------------------------------------------------------------------------------
+// Write a function that determines whether the passed in arrays are similar.Similar means they contain the same elements, and the same number of occurrences of elements.
 
+function arraysSimilar(arr1, arr2) {
+    // check if the length are equal
+    // check if arrays have same value
+    arr1 = arr1.sort((a, b) => a - b)
+    arr2 = arr2.sort((a, b) => a - b)
+
+    return arr1.lenth > arr2.lenth
+        ? arr1.every((value, index) => value === arr2[index])
+        : arr2.every((value, index) => value === arr1[index])
+}
+
+
+const arr1 = [1, 2, 2, 3, 4],
+    arr2 = [2, 1, 2, 4, 3],
+    arr3 = [1, 2, 3, 4],
+    arr4 = [1, 2, 3, "4"]
+
+
+console.log(arraysSimilar(arr1, arr2)); // Should equal true
+console.log(arraysSimilar(arr2, arr3)); // Should equal false
+console.log(arraysSimilar(arr3, arr4)); // Should equal false
 //------------------------------------------------------------------------------------------------------
 // Trolls are attacking your comment section!
 
